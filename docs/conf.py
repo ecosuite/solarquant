068dc03c00c5630a7d6c2920b41501b5bedf610c
HatefulMoron <34914404+HatefulMoron@users.noreply.github.com>
2024-04-18 10:31:36 +1200

    v1.2 (#3)
    Signed-off-by: Thomas Passmore <thomas@ecogysolar.com>

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Ecosuite, Thomas Passmore'
 author = 'Ecosuite, Thomas Passmore'
 
 # The full version, including alpha/beta/rc tags
-release = '1.4'
+release = '1.2'
 
 
 # -- General configuration ---------------------------------------------------
