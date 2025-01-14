e52d237d03f1873c245efce2162fc1a70e1f62ff
Thomas Passmore <thomas@ecosuite.io>
2025-01-14 17:26:09 +1300

    Release 1.2.1
    Signed-off-by: Thomas Passmore <thomas@ecosuite.io>

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Ecosuite, Thomas Passmore'
 author = 'Ecosuite, Thomas Passmore'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2'
+release = '1.2.1'
 
 
 # -- General configuration ---------------------------------------------------
